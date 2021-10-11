04a56f8ebadfca41e8d981bd330183c1fc2e843f
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-10-11 08:39:39 -0400

    mongo stuff

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
